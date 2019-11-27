c42874505acb4858636fbc9d1a06c513d086dccd
DerUberCatman <b.wisman155@gmail.com>
2019-11-27 17:28:36 +0100

    adding bed

--- wemos/src/components.cpp
+++ wemos/src/components.cpp
@@ -11,6 +11,10 @@ void setLed(bool state) {
 ///////////////////
 /// Get Sensors ///
 ///////////////////
-double getForceSensor() {
-    return 0.1;
+unsigned int getForceSensor() {
+    Wire.requestFrom(0x36, 4);
+    unsigned int anin0 = Wire.read() & 0x03;
+    anin0 = anin0 << 8;
+    anin0 = anin0 | Wire.read();
+    return anin0;
 }
