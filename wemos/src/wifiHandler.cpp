057db82dd6c93eee018b856ba7a143ed8c148b09
DerUberCatman <b.wisman155@gmail.com>
2019-11-28 16:49:34 +0100

    stabilized wifi

--- wemos/src/wifiHandler.cpp
+++ wemos/src/wifiHandler.cpp
@@ -34,7 +34,6 @@ void handleWifi(DomObject* object) {
 
     // Check if client is connected
     if (client && client.connected()) {
-        delay(10);
         Serial.println("Client connected");
         delay(60);
 
@@ -61,6 +60,9 @@ void handleWifi(DomObject* object) {
         // Deserializing json
         DynamicJsonDocument doc(1024);
         DeserializationError error = deserializeJson(doc, data);
+        JsonArray actuators = doc["actuators"];
+        
+        object->writeActuators(actuators);
 
         // If there is an error, send error result back to client
         if (error) {
