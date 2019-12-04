9aabebcb37b65cf9da16077270e1b1beb8cb28d1
Straygear <Myth_q3@hotmail.com>
2019-12-04 15:12:42 +0100

    latest tablelamp

--- wemos/src/main.cpp
+++ wemos/src/main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #include <Wire.h>
 #include <Arduino.h>
 #include <string>
+#include <Adafruit_NeoPixel.h>
 #include "domobjects/domobject.h"
 #include "components.h"
 #include "wifiHandler.h"
@@ -15,6 +16,11 @@ Bed object;
 #ifdef CHAIR
 Chair object;
 #endif
+
+
+
+Adafruit_NeoPixel led(1, D5, NEO_GRB + NEO_KHZ800);
+
 
 void setup() {
     Serial.begin(9600);
@@ -35,11 +41,31 @@ void setup() {
     Wire.write(byte(0x03));
     Wire.write(byte(0x0F));
     Wire.endTransmission();
+
+    led.begin();
+    led.show();
+    led.setBrightness(255);
+
 }
 
 void loop() {
     handleWifi(&object);
     delay(20);
     componentCheckLoop();
+
+      //led.show();
+   led.setPixelColor(0, 255, 255, 255);
+   led.show();
+
+
+   Wire.beginTransmission(0x38); 
+  Wire.write(byte(0x00));      
+  Wire.endTransmission();
+  Wire.requestFrom(0x38, 1);   
+  unsigned int inputs = Wire.read();  
+  Serial.print("Digital in: ");
+  Serial.println(inputs&0x0F);  
+
+  delay(500);
 }
 
