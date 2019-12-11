8eaa71ba13eb358c8dcb8e465999554c69e7ac7e
Bertus <b.wisman155@gmail.com>
2019-12-11 21:43:06 +0100

    modified for communicationplan

--- wemos/src/domobjects/fridge.cpp
+++ wemos/src/domobjects/fridge.cpp
@@ -5,22 +5,13 @@ int Fridge::getId() {
     return 6;
 }
 
-void Fridge::getSensors(JsonArray& arr) {
-    JsonObject thermometer1 = arr.createNestedObject();
-    thermometer1["name"] = "thermometer1";
-    thermometer1["value"] = getFridgeTempSensor(0); //Inside temp sensor
-
-    JsonObject thermometer2 = arr.createNestedObject();
-    thermometer2["name"] = "thermometer2";
-    thermometer2["value"] = getFridgeTempSensor(1); //Outside temp sensor
-
-    JsonObject openClose = arr.createNestedObject();
-    openClose["name"] = "openClose";
-    openClose["value"] = getFridgeClicker();
+void Fridge::getSensors(JsonObject& obj) {
+    obj["thermometer1"] = getFridgeTempSensor(0); //Inside temp sensor
+    obj["thermometer2"] = getFridgeTempSensor(1); //Outside temp sensor
+    obj["openClose"] = getFridgeClicker();
 }    
 
-void Fridge::writeActuators(JsonArray& actuators) {
-    JsonObject cooling = actuators[0];
-    int c = cooling["value"];
-    setFridgeActuators(c);
+void Fridge::writeActuators(JsonObject& actuators) {
+    int cooling = actuators["cooling"];
+    setFridgeActuators(cooling);
 }
