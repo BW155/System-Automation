5559b5cd2999e48b2a7fd5c6b36cffc810802daa
zepandasky <zepholleman2012@hotmail.nl>
2019-12-12 13:34:33 +0100

    w

--- raspberry-pi/main.cpp
+++ raspberry-pi/main.cpp
@@ -1,8 +1,14 @@
 #include <stdlib.h>
+#include "timeKeeper.h"
 
 using namespace std;
 
 
 int main(){
-    
+        TimeClass t1;
+    t1.initTimeValues();
+    while(1){
+
+    t1.autoIncreaseTime();
+    }
 }
