939ed0bb0a873f78924b91e7bf18c17e98cbeb6d
zepandasky <zepholleman2012@hotmail.nl>
2019-12-17 13:59:57 +0100

    merge with dev

--- raspberry-pi/main.cpp
+++ raspberry-pi/main.cpp
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-#include <stdlib.h>
-#include "timeKeeper.h"
-#include <time.h>
-using namespace std;
-
-
-int main(){
-    TimeClass t1;
-    t1.initTimeValues();
-    t1.setTime("60", "0", "0", "0");
-    while(1){
-        t1.autoIncreaseTime();
-        int *p = t1.getTime();
-        t1.setTime("11", "cur", "cur" , "cur");
-    }
-=======
 // Lars Hartog
 #include "includes.h"
 
@@ -370,5 +353,4 @@ int main(int argc, char const *argv[])
         }
     }
     return 0;
->>>>>>> development
 }
