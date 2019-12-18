1d63ccb6c60774f283050ae99f0c334f8c3cdfc2
DerUberCatman <b.wisman155@gmail.com>
2019-12-18 15:43:00 +0100

    added socket server

--- front-end/data_socket.py
+++ front-end/data_socket.py
@@ -1,12 +1,13 @@
-import _thread as thread
+import threading
 from time import sleep
+import socket
+import json
+from system_automation.objects import objects
 
 bind_ip = '127.0.0.1'
 bind_port = 9001
+BUFFER_SIZE = 1200
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((bind_ip, bind_port))
-server.listen(5)  # max backlog of connections
 
 """
 Type:
@@ -15,11 +16,43 @@ Type:
 3 = Receive All
 """
 def data_socket_routine():
-    while 1:
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+    server.bind((bind_ip, bind_port))
+    server.listen(1)
 
+    while 1:
+        conn, addr = server.accept()
+        data = conn.recv(BUFFER_SIZE)
+        if not data: break
+        data = data.decode('utf-8')
+        print("received data: ", data)
+        resp = process_message(data)
+        conn.send(bytes(resp, "utf-8"))
         sleep(0.5) # sleep 1 second
 
 
+def process_message(message):
+    global objects
+    message = json.loads(message)
+    messsage_type = message.get("type")
+
+    if not messsage_type:
+        return "Error"
+
+    if messsage_type == 1:
+        return "1"
+
+    if messsage_type == 2:
+        return json.dumps(objects)
+
+    if messsage_type == 3:
+        objects = message["data"]
+        return "1"
+
+
 def start_data_socket_thread():
-    thread.start_new_thread(data_socket_routine, ())
+    t = threading.Thread(target=data_socket_routine)
+    t.daemon = True
+    t.start()
 
