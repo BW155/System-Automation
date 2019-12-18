bf94757fb4d16a3fe59dd47b4fcd1a52f29f683e
DerUberCatman <b.wisman155@gmail.com>
2019-12-18 17:02:45 +0100

    made changed to socket server

--- front-end/data_socket.py
+++ front-end/data_socket.py
@@ -5,7 +5,7 @@ import json
 from json import JSONDecodeError
 from system_automation.objects import objects, set_objects, check_objects_change
 
-bind_ip = '127.0.0.1'
+bind_ip = '0.0.0.0'
 bind_port = 9001
 BUFFER_SIZE = 1200
 
@@ -20,17 +20,24 @@ def data_socket_routine():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
     server.bind((bind_ip, bind_port))
-    server.listen(1)
+    server.listen(10)
 
     while 1:
+        print("accepting")
         conn, addr = server.accept()
+        print("accepted: {}".format(addr))
+        print("receiving")
         data = conn.recv(BUFFER_SIZE)
+        print("received")
         if not data: break
         message = data.decode('utf-8')
         print("received data: ", message)
         resp = process_message(message)
+        print("sending")
         conn.send(bytes(resp, "utf-8"))
-        sleep(0.5) # sleep 1 second
+        print("send")
+        conn.close()
+        print("closed")
 
 
 def process_message(message):
@@ -50,7 +57,9 @@ def process_message(message):
         return "1" if check_objects_change() else "0"
 
     if messsage_type == 2:
-        return json.dumps(objects)
+        obj_id = js.get("id")
+        if obj_id and obj_id >= 1 and obj_id <= 7:
+            return json.dumps([i for i in objects if i["id"] == obj_id][0])
 
     if messsage_type == 3:
         data = js.get("data")
