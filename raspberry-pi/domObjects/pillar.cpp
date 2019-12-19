6780d90bafeaf5951b7dcc7f75041b5fdc5131f7
Fenrir204 <steyn.veltmeijer@gmail.com>
2019-12-19 15:26:04 +0100

    pi pillar

--- raspberry-pi/domObjects/pillar.cpp
+++ raspberry-pi/domObjects/pillar.cpp
@@ -1,4 +1,86 @@
 //
 // Created by LarsLinux on 16-12-19.
 //
+#include "pillar.h"
+#include "domObject.h"
 
+using namespace std;
+using json = nlohmann::json;
+
+Pillar::Pillar(char* IP, webSocket *x) : domObject(x){
+    gassensor = 0;
+    button = 0;
+    led = 0;
+    buzzer = 0;
+    Socket temp(4, "pillar", IP);
+    domObject::wemos = temp;
+}
+
+char* Pillar::wemosMessage(json Result){
+    button = Result["sensors"]["button"];
+    gassensor = Result["sensor"]["gasSensor"];
+}
+
+char* Pillar::pythonMessage(bool Led, bool buz) {
+    json Message = {
+            {"id",4},
+            {"actuators", {
+                        {"led", led},
+                        {"buzzer", buzzer}
+                    }
+            }
+        };
+    char *message = toCharArray(Message);
+    return message;
+}
+
+void Pillar::update(){
+    char *result;
+    if(python->sendMessage(4)) { //als er verandering is
+        result = python->receiveActuators(4);
+    }
+    json Result = toJson(result);
+
+    buzzer = Result["actuators"]["buzzer"];
+    led = Result["actuators"]["led"];
+
+    json wemosMessage = {
+            {"id",4},
+            {"actuators", {
+                        {"led", led},
+                        {"buzzer", buzzer}
+                    }
+            }
+    };
+    char *wemos_message = toCharArray(wemosMessage);
+        
+    char* receive_sensor = wemos.sendReceive(wemos_message);
+    json ReceiveSensor = toJson(receive_sensor);
+
+    if(gassensor == !ReceiveSensor["sensors"]["gasSensor"] || button == !ReceiveSensor["sensors"]["button"]){
+        gassensor = ReceiveSensor["sensors"]["gasSensor"];
+        button = ReceiveSensor["sensors"]["button"];
+
+        buzzer = (gassensor >= 875);
+        
+        json python_Message = {
+                {"id",4},
+                {"actuators", {
+                            {"led", led},
+                            {"buzzer", buzzer}
+                        }
+                },
+                {"sensors", {
+                            {"gasSensor", gassensor},
+                            {"button", button}
+                    }
+                }
+        };
+        char *python_message = toCharArray(python_Message);
+        python->sendAll(4, *python_message); // stuur alle sensors, alleen als uit sendReceive blijkt dat er veranderingen zijn
+    }
+}
+
+bool Pillar::get_buzzer(){
+    return buzzer;
+}  
--- raspberry-pi/domObjects/pillar.h
+++ raspberry-pi/domObjects/pillar.h
@@ -6,11 +6,27 @@
 #define SOCKET_BED_H
 
 #include "../Socket/Socket.h"
+#include <iostream>
+#include <string>
+#include "../json/json.hpp"
+#include "../Socket/webSocket.h"
+#include "domObject.h"
+using namespace std;
 
-class pillar {
+class door;
+
+class Pillar : public domObject{
 private:
-
+    int gassensor;
+    bool button;
+    bool led;
+    bool buzzer;
+    Socket socket;
 public:
-
+    Pillar(char*, webSocket*);
+    bool get_buzzer();
+    virtual char* wemosMessage(json){};
+    virtual char* pythonMessage(bool, bool){};
+    virtual void update();
 };
 #endif //SOCKET_BED_H
