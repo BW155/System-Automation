b34c1ffa9e6d8292fba5d5e2e74a46c3807136b5
zepandasky <zepholleman2012@hotmail.nl>
2019-12-20 13:55:08 +0100

    Timeclass obj is global

--- raspberry-pi/main.cpp
+++ raspberry-pi/main.cpp
@@ -10,15 +10,15 @@ using namespace std;
 
 webSocket pyt;
 vector<domObject> objects;
+TimeClass obj1 (10,0,0,0);
 
 bool checkConnectedDevices() {
     return objects.size() == 7;
 }
 
 void discoverDevices() {
-    TimeClass obj1 (10,0,0,0);
-
     
+
     const char *IPGROUP = "192.168.2.";
     char *IP;
     int Device;
