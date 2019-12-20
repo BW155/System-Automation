259d4a9093862d17ef2eff0d6c3d982e1efcefc8
DerUberCatman <b.wisman155@gmail.com>
2019-12-20 11:43:05 +0100

    fixed notifications issue

--- front-end/system_automation/api.py
+++ front-end/system_automation/api.py
@@ -14,6 +14,7 @@ notification_templates = [
 
 notifications = []
 
+
 # api route for logged-in users
 @app.route("/api/dashboard/<int:obj_id>", methods=["POST", "GET"])
 @login_required
@@ -38,26 +39,20 @@ def interface_api(action):
                 return "1"
     return "0"
 
-
 @app.route("/api/interface_notifications/<int:notif_type>", methods=["POST"])
 def interface_notifications_post(notif_type):
     global notifications
 
-    if len(notifications) > 0:
-        not_id = notifications[-1]["not_id"] + 1
-    else:
-        not_id = 0
-
     if notif_type == 0:
         notif = notification_templates[0]
         notif["time"] = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-        notif["not_id"] = not_id
-        notifications.append(notif)
+        notif["not_id"] = len(notifications)
+        notifications.append(notif.copy())
     elif notif_type == 1:
         notif = notification_templates[1]
         notif["time"] = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-        notif["not_id"] = not_id
-        notifications.append(notif)
+        notif["not_id"] = len(notifications)
+        notifications.append(notif.copy())
     else:
         return "0"
     return "1"
@@ -67,19 +62,21 @@ def interface_notifications_post(notif_type):
 @login_required
 @roles_allowed([Role.GUARD])
 def interface_notifications():
-    global notifications
-    cur_notifications = notifications
-    not_id = flask.request.args.get("not_id")
-    if not_id.isdigit():
-        cur_notifications = [n for n in notifications if n["not_id"] > int(not_id)]
+    cur_notifications = []
+    not_id = -1
 
+    req_not_id = flask.request.args.get("not_id")
+    if req_not_id.isdigit():
+        not_id = int(req_not_id)
+
+    cur_notifications = [n for n in notifications if n["not_id"] > int(not_id)]
     cur_time = time.time()
 
     while len(cur_notifications) == 0:
-        time.sleep(0.2)
+        cur_notifications = [n for n in notifications if n["not_id"] > int(not_id)]
+        time.sleep(0.1)
         if time.time() - cur_time >= 9:
             return "TimeOut", 308
 
-    print(cur_notifications)
     return json.dumps(cur_notifications)
 
