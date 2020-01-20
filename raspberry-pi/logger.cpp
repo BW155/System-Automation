aa96d71e624c974daa9ec75ea5a8205ef5eb5c29
DerUberCatman <b.wisman155@gmail.com>
2020-01-20 12:54:02 +0100

    removed trash

--- raspberry-pi/logger.cpp
+++ raspberry-pi/logger.cpp
@@ -4,20 +4,13 @@ using namespace std;
 
 
 void logToFile(TimeClass *timeObject, string text) {
-    //createIfNotExist(FILE_NAME);
-
-    /*ofstream outfile;
-    outfile.open(FILE_NAME, ios_base::app); // append instead of overwrite
-    outfile << timeObject->getTimeString() << " | " << text; 
-    outfile.close();*/
-
+    createIfNotExist(FILE_NAME);
     ofstream fout;  // Create Object of Ofstream
     ifstream fin;
     fin.open(FILE_NAME);
     fout.open(FILE_NAME, ios::app); // Append mode
     if(fin.is_open())
         fout << timeObject->getTimeString() << " | " << text << endl; 
-    cout<<"\n Data has been appended to file";
     fin.close();
     fout.close(); // Closing the file
 }
